433250ed9ef3a116341e3a0d580959da14b93c1a
Dpipeto <dpipegarzon@gmail.com>
2026-04-08 21:53:49 -0500

    intento de diseño junto a correcciones

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    a=1
-    return "Hello, World!"
-@app.route('/inicio/')
 def index():
     return render_template('index.html')
 @app.route('/Generos/')
